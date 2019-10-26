1f36e9cd9af7790fbc7034069dfc7cc8ffeab098
francescoConforti <francesco.conforti.04@gmail.com>
2019-10-26 18:55:01 +0200

    point 3 stub

--- partie2/TD3/main.cpp
+++ partie2/TD3/main.cpp
@@ -16,6 +16,9 @@ Mat image;    // working image
 vector<Point> germes;
 
 void germeSelect(int event, int x, int y, int flags, void* userdata);
+bool predicat(const Mat img, Point g, Point p, int seuil);
+void croissanceRegion(const Mat src, Mat dst, vector<Point> germes, int seuil);
+vector<Point> findNeighbours(const Mat img, Point p);
 
 int main( int argc, char** argv )
 {
@@ -46,4 +49,37 @@ void germeSelect(int event, int x, int y, int flags, void* userdata){
   germes.push_back(Point(x, y));
   circle(imgcolor, germes.back(), 3, Scalar(0, 255, 0), 5);
   imshow( window_name, imgcolor );
+}
+
+bool predicat(const Mat img, Point g, Point p, int seuil){
+  bool res = false;
+  uchar intensity_p = img.at<uchar>(p.y, p.x);
+  uchar intensity_g = img.at<uchar>(g.y, g.x);
+  if(abs(intensity_p - intensity_g) < seuil){
+    res = true;
+  }
+  return res;
+}
+
+// dst should be initialised to 0
+void croissanceRegion(const Mat src, Mat dst, vector<Point> germes, int seuil){
+  int color = 1;
+  for(unsigned int i=0; i < germes.size(); ++i){
+    dst.at<uchar>(germes[i].y, germes[i].x) = color;
+    vector<Point> neighbours = findNeighbours(src, germes[i]); // TODO
+    while(!neighbours.empty()){
+      Point p = neighbours.front();
+      if(dst.at<uchar>(p.y, p.x) == 0 && predicat(src, germes[i], p, seuil)){
+        dst.at<uchar>(p.y, p.x) = color;
+        // TODO: add neighbours
+      }
+    }
+    color += 10;
+  }
+}
+
+// TODO
+vector<Point> findNeighbours(const Mat img, Point p){
+  vector<Point> res;
+  return res;
 }
