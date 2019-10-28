8e8a0de94d2466690df089e89243373dbd330974
francescoConforti <francesco.conforti.04@gmail.com>
2019-10-28 17:26:50 +0100

    working region segmentation

--- partie2/TD3/main.cpp
+++ partie2/TD3/main.cpp
@@ -9,8 +9,9 @@
 using namespace cv;
 using namespace std;
 
-string imageName("lotus.png");          // Image by default
-string window_name("image");
+
+string window_name("Image originale");
+string window_name2("Image segmentée");
 Mat imgcolor; // To show the image with colored germes
 Mat image;    // working image
 vector<Point> germes;
@@ -19,26 +20,42 @@ void germeSelect(int event, int x, int y, int flags, void* userdata);
 bool predicat(const Mat img, Point g, Point p, int seuil);
 void croissanceRegion(const Mat src, Mat dst, vector<Point> germes, int seuil);
 vector<Point> findNeighbours(const Mat img, Point p);
+bool insideImage(const Mat img, Point p);
 
 int main( int argc, char** argv )
 {
+  int seuil = 10;
+  string imageName("rice.png");
+  
   if( argc > 1)
   {
     imageName = argv[1];
   }
 
-  imgcolor = imread(imageName.c_str(), IMREAD_COLOR); // Read the file
-  if( imgcolor.empty() )                          // Check for invalid input
+  imgcolor = imread(imageName.c_str(), IMREAD_COLOR); // color for germe highlighting
+  if( imgcolor.empty() )
   {
     std::cout <<  "Could not open or find the image" << std::endl ;
     return -1;
   }
+  if(argc > 2){
+    sscanf(argv[2], "%d", &seuil);
+  }
+  
   cvtColor(imgcolor,image,COLOR_BGR2GRAY);
   
   namedWindow( window_name, WINDOW_AUTOSIZE );
   imshow( window_name, imgcolor );
   
   setMouseCallback(window_name, germeSelect );
+  waitKey(0);
+  
+  Mat dst = Mat(image.size(), CV_8UC1, Scalar(0));
+  croissanceRegion(image, dst, germes, seuil);
+  
+  imshow(window_name, image);
+  namedWindow(window_name2, WINDOW_AUTOSIZE);
+  imshow(window_name2, dst);
   waitKey(0);
 }
 
@@ -63,23 +80,39 @@ bool predicat(const Mat img, Point g, Point p, int seuil){
 
 // dst should be initialised to 0
 void croissanceRegion(const Mat src, Mat dst, vector<Point> germes, int seuil){
-  int color = 1;
+  int color = 10;
   for(unsigned int i=0; i < germes.size(); ++i){
     dst.at<uchar>(germes[i].y, germes[i].x) = color;
-    vector<Point> neighbours = findNeighbours(src, germes[i]); // TODO
+    vector<Point> neighbours = findNeighbours(src, germes[i]);
     while(!neighbours.empty()){
       Point p = neighbours.front();
+      neighbours.erase(neighbours.begin());
       if(dst.at<uchar>(p.y, p.x) == 0 && predicat(src, germes[i], p, seuil)){
+        cout << "p: " << p.x << ", " << p.y << "\n";
         dst.at<uchar>(p.y, p.x) = color;
-        // TODO: add neighbours
+        vector<Point> tmp = findNeighbours(src, p);
+        neighbours.insert(neighbours.end(), tmp.begin(), tmp.end());
       }
     }
     color += 10;
   }
 }
 
-// TODO
 vector<Point> findNeighbours(const Mat img, Point p){
   vector<Point> res;
+  for(int x = -1; x <= 1; ++x){
+    for(int y = -1; y <= 1; ++y){
+      if(x != 0 && y != 0){ // already have this point
+        Point newPoint = Point(p.x + x, p.y + y);
+        if(insideImage(img, newPoint)){
+          res.push_back(newPoint);
+        }
+      }
+    }
+  }
   return res;
+}
+
+bool insideImage(const Mat img, Point p){
+  return p.x >= 0 && p.x < img.size().width && p.y >= 0 && p.y < img.size().height;
 }
