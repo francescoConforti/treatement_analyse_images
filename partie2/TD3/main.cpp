c1e60c5a3306b7000253d611ac9f49126cb7a7a9
francescoConforti <francesco.conforti.04@gmail.com>
2019-10-28 18:54:52 +0100

    trackbar

--- partie2/TD3/main.cpp
+++ partie2/TD3/main.cpp
@@ -14,17 +14,19 @@ string window_name("Image originale");
 string window_name2("Image segmentée");
 Mat imgcolor; // To show the image with colored germes
 Mat image;    // working image
+Mat dst;
 vector<Point> germes;
+int seuil = 10;
 
 void germeSelect(int event, int x, int y, int flags, void* userdata);
 bool predicat(const Mat img, Point g, Point p, int seuil);
 void croissanceRegion(const Mat src, Mat dst, vector<Point> germes, int seuil);
 vector<Point> findNeighbours(const Mat img, Point p);
 bool insideImage(const Mat img, Point p);
+void selectionnerSeuil(int, void*);
 
 int main( int argc, char** argv )
 {
-  int seuil = 10;
   string imageName("rice.png");
   
   if( argc > 1)
@@ -50,12 +52,13 @@ int main( int argc, char** argv )
   setMouseCallback(window_name, germeSelect );
   waitKey(0);
   
-  Mat dst = Mat(image.size(), CV_8UC1, Scalar(0));
+  dst = Mat(image.size(), CV_8UC1, Scalar(0));
   croissanceRegion(image, dst, germes, seuil);
   
   imshow(window_name, image);
   namedWindow(window_name2, WINDOW_AUTOSIZE);
   imshow(window_name2, dst);
+  createTrackbar("seuil", window_name2, &seuil, 100, selectionnerSeuil);
   waitKey(0);
 }
 
@@ -88,7 +91,6 @@ void croissanceRegion(const Mat src, Mat dst, vector<Point> germes, int seuil){
       Point p = neighbours.front();
       neighbours.erase(neighbours.begin());
       if(dst.at<uchar>(p.y, p.x) == 0 && predicat(src, germes[i], p, seuil)){
-        cout << "p: " << p.x << ", " << p.y << "\n";
         dst.at<uchar>(p.y, p.x) = color;
         vector<Point> tmp = findNeighbours(src, p);
         neighbours.insert(neighbours.end(), tmp.begin(), tmp.end());
@@ -115,4 +117,10 @@ vector<Point> findNeighbours(const Mat img, Point p){
 
 bool insideImage(const Mat img, Point p){
   return p.x >= 0 && p.x < img.size().width && p.y >= 0 && p.y < img.size().height;
+}
+
+void selectionnerSeuil(int, void*){
+  dst = Mat(image.size(), CV_8UC1, Scalar(0));
+  croissanceRegion(image, dst, germes, seuil);
+  imshow(window_name2, dst);
 }
